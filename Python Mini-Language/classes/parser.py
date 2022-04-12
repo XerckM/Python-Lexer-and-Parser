f81ab95377ff9e6da7b76a4ecd2d3c5fc1a44a67
Zeke <67399106+XerckM@users.noreply.github.com>
2022-04-12 00:08:45 -0400

    few functions added

--- Python Mini-Language/classes/parser.py
+++ Python Mini-Language/classes/parser.py
@@ -1,24 +1,77 @@
 from classes.lexer import Lexer
-from classes.error import IllegalSyntaxError
+from classes.error import InvalidSyntaxError
+
 
 ######################
 # PARSER
 ######################
 
-# (Needs more lines of codes...)
+
 class Parser:
     def __init__(self, text):
+        self.tok_pos = None
+        self.tok_line = None
+        self.tok_val_len = None
         self.lex = Lexer(text)
+        self.token = None
+
+    def parse(self):
+        return self.program()
+
+    def program(self):
         self.token = self.lex.next()
+        match self.token.kind():
+            case 'COMMENT':
+                return self.program()
+            case 'program':
+                return self.identifier()
+            case 'end':
+                return True
+            case _:
+                return InvalidSyntaxError(self.token.position())
 
-    def parse_program(self):
+    def identifier(self):
+        self.token = self.lex.next()
+        if self.token.kind() == 'ID':
+            self.tok_pos = self.token.position_
+            self.tok_line = self.token.line
+            self.tok_val_len = len(self.token.value_)
+            return self.close_tok()
+        else:
+            return InvalidSyntaxError(self.token.position())
+
+    def close_tok(self):
+        self.token = self.lex.next()
+        match self.token.kind():
+            case ':':
+                return self.body()
+            case ';':
+                return self.body()
+            case _:
+                return InvalidSyntaxError(f'(Line: {self.tok_line}, {self.tok_val_len + self.tok_pos})')
+
+    def body(self):
+        # return 'Program Body here!'
+        self.token = self.lex.next()
+        match self.token.kind():
+            case ('int' | 'bool'):
+                return self.declarations()
+            case 'ID':
+                return self.assign_stmt()
+            case 'print':
+                return self.print_stmt()
+            case _:
+                return InvalidSyntaxError(self.token.position())
+
+    def print_stmt(self):
+        self.token = self.lex.next()
+        match self.token.kind():
+            case
+            case _:
+                return InvalidSyntaxError(self.token.position())
+
+    def declarations(self):
+        return 'Declarations!'
+
+    def declaration(self):
         pass
-
-    def is_identifier(self):
-        ok = False
-        try:
-            if self.token.kind() == 'ID':
-                ok = True
-        except AttributeError:
-            pass
-        return ok
