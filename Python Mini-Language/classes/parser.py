ef050b42244c2c67c4d63024f617fc9d4824dc03
Zeke <67399106+XerckM@users.noreply.github.com>
2022-04-13 13:17:08 -0400

    updated parser

--- Python Mini-Language/classes/parser.py
+++ Python Mini-Language/classes/parser.py
@@ -1,6 +1,4 @@
 from classes.lexer import Lexer
-from classes.error import InvalidSyntaxError
-
 
 ######################
 # PARSER
@@ -9,69 +7,147 @@ from classes.error import InvalidSyntaxError
 
 class Parser:
     def __init__(self, text):
-        self.tok_pos = None
-        self.tok_line = None
-        self.tok_val_len = None
         self.lex = Lexer(text)
-        self.token = None
+        self.token = self.lex.next()
+        self.op = None
 
     def parse(self):
         return self.program()
 
-    def program(self):
-        self.token = self.lex.next()
-        match self.token.kind():
-            case 'COMMENT':
-                return self.program()
-            case 'program':
-                return self.identifier()
-            case 'end':
-                return True
-            case _:
-                return InvalidSyntaxError(self.token.position())
-
-    def identifier(self):
-        self.token = self.lex.next()
-        if self.token.kind() == 'ID':
-            self.tok_pos = self.token.position_
-            self.tok_line = self.token.line
-            self.tok_val_len = len(self.token.value_)
-            return self.close_tok()
+    def match(self, value):
+        if self.token.kind() == value:
+            self.token = self.lex.next()
         else:
-            return InvalidSyntaxError(self.token.position())
+            return f'<At {self.token.position()} I see {self.token.kind()} \
+                   but expected {value}>'
 
-    def close_tok(self):
-        self.token = self.lex.next()
-        match self.token.kind():
-            case ':':
-                return self.body()
-            case ';':
-                return self.body()
-            case _:
-                return InvalidSyntaxError(f'(Line: {self.tok_line}, {self.tok_val_len + self.tok_pos})')
+    def program(self):
+        while self.token.kind() == 'COMMENT':
+            self.match('COMMENT')
+        self.match('program')
+        self.match('ID')
+        self.match(':')
+        self.body()
+        self.match('end')
+        print('True')
 
     def body(self):
-        # return 'Program Body here!'
-        self.token = self.lex.next()
-        match self.token.kind():
-            case ('int' | 'bool'):
-                return self.declarations()
-            case 'ID':
-                return self.assign_stmt()
-            case 'print':
-                return self.print_stmt()
-            case _:
-                return InvalidSyntaxError(self.token.position())
-
-    def print_stmt(self):
-        self.token = self.lex.next()
-        match self.token.kind():
-            case
-            case _:
-                return InvalidSyntaxError(self.token.position())
+        if self.token.kind() == ('bool' or 'int'):
+            self.declarations()
+        self.statements()
 
     def declarations(self):
-        return 'Declarations!'
+        self.declaration()
+        while self.token.kind() == ('bool' or 'int'):
+            self.declaration()
 
     def declaration(self):
-        pass
+        assert self.token.kind() == ('bool' or 'int'), f'Expected \
+               "bool" or "int" instead found {self.token.kind()}'
+        self.token = self.lex.next()
+        self.match('ID')
+        self.match(';')
+
+    def statements(self):
+        self.statement()
+        while self.token.kind() == ';':
+            self.token = self.lex.next()
+            self.statement()
+
+    def statement(self):
+        match self.token.kind():
+            case 'ID':
+                self.assignment_statement()
+            case 'if':
+                self.conditional_statement()
+            case 'while':
+                self.iterative_statement()
+            case 'print':
+                self.print_statement()
+            case _:
+                self.expected(['if', 'ID', 'while', 'print'])
+
+    def expected(self, type_list):
+        if self.token.kind() not in type_list:
+            return f'ERROR! at {self.token.position()}. Expected to see \
+                   {type_list}, but see {self.token.kind()}'
+
+    def assignment_statement(self):
+        assert self.token.kind() == 'ID', f'Expected "ID" instead found {self.token.kind()}'
+        self.match('ID')
+        self.match(':=')
+        self.expr()
+
+    def conditional_statement(self):
+        self.match('if')
+        self.expr()
+        self.match('then')
+        self.body()
+        if self.token.kind() == 'else':
+            self.body()
+        self.match('fi')
+
+    def expr(self):
+        self.simple_expr()
+        if self.token.kind() in ['<', '>', '<=', '>=', '!=', '=']:
+            self.token = self.lex.next()
+            self.simple_expr()
+
+    def simple_expr(self):
+        self.term()
+        while self.token.kind() is ('+' or '-' or 'or'):
+            self.token = self.lex.next()
+            self.term()
+
+    # Needs fixing
+    def term(self):
+        self.factor()
+        while self.token.kind() is ('*' or '/' or 'and'):
+            op = self.token.kind()
+            self.token = self.lex.next()
+            t = self.factor()
+
+    def factor(self):
+        match self.token.kind():
+            case ('+' | '-'):
+                tok = self.token.kind()
+                self.token = self.lex.next()
+                return tok
+            case ('true' | 'false' | 'NUM'):
+                tok = self.token.kind()
+                self.literal()
+                return tok
+            case 'ID':
+                self.token = self.lex.next()
+            case '(':
+                tok = self.token.kind()
+                self.token = self.lex.next()
+                self.expr()
+                self.match(')')
+            case _:
+                self.expected(['true', 'false', 'NUM', 'ID', ')'])
+
+    def literal(self):
+        assert self.token.kind() == ('true' or 'false' or 'NUM'), \
+            f'Expected "true" or "false" or "NUM" instead found {self.token.kind()}'
+        match self.token.kind():
+            case ('true' | 'false'):
+                self.boolean_literal()
+            case 'NUM':
+                self.token = self.lex.next()
+
+    def boolean_literal(self):
+        assert self.token.kind() == ('true' or 'false'), \
+            f'Expected "true" or "false" instead found {self.token.kind()}'
+        self.token = self.lex.next()
+
+    def iterative_statement(self):
+        self.match('while')
+        self.expr()
+        self.match('do')
+        self.body()
+
+    def print_statement(self):
+        assert self.token.kind() == 'print', f'Expected "print" instead found {self.token.kind()}'
+        self.token = self.lex.next()
+        self.expr()
